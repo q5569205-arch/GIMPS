91700febb89f15697a84fe5989ab2b8f0a4145f1
q5569205-arch <q5569205@gmail.com>
2025-11-18 11:23:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import subprocess
+import time
+
 from keep_alive import keep_alive
 
 keep_alive()
 
-try:
-  subprocess.run(["./mprime","-m"])
-except:
-  subprocess.run(["chmod","777","./mprime"])
-  subprocess.run(["./mprime", "-m"])
+p = subprocess.Popen(["./GIMPS/mprime", "-m"], stdin=subprocess.PIPE)
+time.sleep(5)
+p.stdin.write("4\n".encode())
+p.stdin.flush()
+
+while True:
+  input()
