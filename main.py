0a2cd93792637da16e2fefe4423f9ee956ea1cb2
q5569205-arch <q5569205@gmail.com>
2025-11-18 11:34:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from keep_alive import keep_alive
 
 keep_alive()
 
-p = subprocess.Popen(["./GIMPS/mprime", "-m"], stdin=subprocess.PIPE)
+subprocess.run(["chmod", "777", "./mprime"])
+p = subprocess.Popen(["./mprime", "-m"], stdin=subprocess.PIPE)
 time.sleep(5)
 p.stdin.write("4\n".encode())
 p.stdin.flush()
